48a24f65943a3f6234aa8a5eea68ea33bbe10c0a
maah635 <m>
2026-07-21 19:20:49 -0300

    update adicionar.js

--- adicionar.js
+++ adicionar.js
@@ -1,5 +1,43 @@
 import {
-    db
+    db,
+    collection,
+    addDoc
 } from "./firebase.js";
 
-alert("Firebase carregou!");
+document
+.getElementById("salvar")
+.onclick =
+async ()=>{
+
+    try{
+
+        await addDoc(
+
+            collection(
+                db,
+                "teste"
+            ),
+
+            {
+                nome:"Teste"
+            }
+
+        );
+
+        alert(
+            "Salvou!"
+        );
+
+    }catch(erro){
+
+        alert(
+            erro.message
+        );
+
+        console.log(
+            erro
+        );
+
+    }
+
+};
