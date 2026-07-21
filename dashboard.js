344128da6296327cdf93677f3dc556618d7bf94a
maah635 <marcellacampos954@gmail.com>
2026-07-20 21:21:35 -0300

    update

--- dashboard.js
+++ dashboard.js
@@ -1,33 +1,3 @@
-import {
-    verificarLogin,
-    logout
-} from "./auth.js";
-
-const usuario =
-verificarLogin();
-
-const musicas = 4285;
-const favoritas = 84;
-const erros = 3;
-
-document.getElementById("totalMusicas").innerText =
-musicas;
-
-document.getElementById("favoritas").innerText =
-favoritas;
-
-document.getElementById("erros").innerText =
-erros;
-
-document
-.getElementById("adicionar")
-.addEventListener("click", ()=>{
-
-    window.location.href =
-    "musicas.html";
-
-});
-
 const usuario =
 JSON.parse(
     localStorage.getItem(
@@ -35,18 +5,152 @@ JSON.parse(
     )
 );
 
-if(usuario){
+if(!usuario){
 
-    document.querySelector(
-        "h1"
-    ).innerText =
-    `Olá, ${usuario.nome}!`;
+    window.location.href =
+    "index.html";
 
 }
 
 document
-.getElementById("logout")
+.getElementById(
+    "nomeUsuario"
+)
+.innerText =
+`Olá, ${usuario.nome}!`;
+
+const musicas =
+JSON.parse(
+    localStorage.getItem(
+        "musicas"
+    )
+) || [];
+
+const favoritas =
+musicas.filter(
+    musica =>
+    musica.favorita
+);
+
+const erros =
+musicas.filter(
+    musica =>
+    musica.erro
+);
+
+document
+.getElementById(
+    "totalMusicas"
+)
+.innerText =
+musicas.length;
+
+document
+.getElementById(
+    "favoritas"
+)
+.innerText =
+favoritas.length;
+
+document
+.getElementById(
+    "erros"
+)
+.innerText =
+erros.length;
+
+const lista =
+document.getElementById(
+    "lista"
+);
+
+lista.innerHTML = "";
+
+if(
+    musicas.length === 0
+){
+
+    lista.innerHTML =
+    "<li>Nenhuma música adicionada.</li>";
+
+}else{
+
+    const ultimas =
+    musicas
+    .slice(-3)
+    .reverse();
+
+    ultimas.forEach(
+        musica =>{
+
+            const item =
+            document
+            .createElement(
+                "li"
+            );
+
+            item.innerText =
+            musica.nome;
+
+            lista.appendChild(
+                item
+            );
+
+        }
+    );
+
+}
+
+document
+.getElementById(
+    "logout"
+)
 .addEventListener(
     "click",
-    logout
+    ()=>{
+
+        localStorage.removeItem(
+            "usuario"
+        );
+
+        window.location.href =
+        "index.html";
+
+    }
+);
+
+document
+.getElementById(
+    "adicionar"
+)
+.addEventListener(
+    "click",
+    ()=>{
+
+        window.location.href =
+        "musicas.html";
+
+    }
+);
+
+const menu =
+document.getElementById(
+    "menu"
+);
+
+const menuLateral =
+document.getElementById(
+    "menuLateral"
+);
+
+menu.addEventListener(
+    "click",
+    ()=>{
+
+        menuLateral.classList
+        .toggle(
+            "ativo"
+        );
+
+    }
 );
