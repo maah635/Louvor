b0bed82d042cd7f1b14f98f49b8ffec60db058f2
maah635 <marcellacampos954@gmail.com>
2026-07-20 21:32:07 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-alert("APP NOVO");
+import {
+    auth,
+    provider,
+    signInWithPopup
+} from "./firebase.js";
+
+const botao =
+document.getElementById(
+    "googleLogin"
+);
+
+botao.addEventListener(
+    "click",
+    async ()=>{
+
+        try{
+
+            const resultado =
+            await signInWithPopup(
+                auth,
+                provider
+            );
+
+            const usuario =
+            resultado.user;
+
+            localStorage.setItem(
+                "usuario",
+                JSON.stringify({
+                    nome:
+                    usuario.displayName,
+
+                    email:
+                    usuario.email,
+
+                    foto:
+                    usuario.photoURL
+                })
+            );
+
+            window.location.href =
+            "dashboard.html";
+
+        }catch(erro){
+
+            console.log(erro);
+
+            alert(
+                erro.message
+            );
+
+        }
+
+    }
+);
