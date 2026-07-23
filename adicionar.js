119f69b4ee770a9e03d9e4d4a9985f8b90dc8a66
maah635 <marcellacampos954@gmail.com>
2026-07-23 00:03:25 -0300

    update adicionar.js

--- adicionar.js
+++ adicionar.js
@@ -1,28 +1,10 @@
 import {
     db,
-    collection,
-    addDoc
+    collection
 } from "./firebase.js";
 
-document.getElementById("salvar")
-.onclick = async () => {
+alert("db:");
+alert(typeof db);
 
-    alert("1");
-
-    try {
-
-        await addDoc(
-            collection(db, "teste"),
-            {
-                nome: "Teste pelo App"
-            }
-        );
-
-        alert("2");
-
-    } catch (e) {
-
-        alert(e.message);
-
-    }
-};
+alert("collection:");
+alert(typeof collection);
