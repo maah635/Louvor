46035adba63dd4fc00490894404391b795618e5d
maah635 <marcellacampos954@gmail.com>
2026-07-23 02:13:18 -0300

    update

--- adicionar.js
+++ adicionar.js
@@ -1,42 +1,9 @@
-// adicionar.js
-import {
-  db,
-  collection,
-  addDoc
-} from "./firebase.js";
+alert("JS CARREGOU");
 
 const botao = document.getElementById("salvar");
 
-botao.addEventListener("click", async () => {
+alert(botao);
 
-    alert("1");
-
-    const nome = document.getElementById("nome").value;
-    const cantor = document.getElementById("cantor").value;
-    const letra = document.getElementById("letra").value;
-
-    if (!nome || !cantor) {
-        alert("Preencha os campos");
-        return;
-    }
-
-    try {
-
-        alert("2");
-
-        await addDoc(collection(db, "musicas"), {
-            nome,
-            cantor,
-            letra
-        });
-
-        alert("3 - SALVOU!");
-
-    } catch (erro) {
-
-        alert("ERRO:");
-        alert(erro.message);
-
-    }
-
-});
+botao.onclick = () => {
+    alert("CLIQUE FUNCIONOU");
+};
